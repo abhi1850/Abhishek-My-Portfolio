cde9c11f5307d1a7d7e176216857d25c5472fd58
Abhishek T <abhishekthyagaraj@gmail.com>
2024-08-03 07:11:48 +0530

    done deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    host: '0.0.0.0',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
+        },
+    },
 });
