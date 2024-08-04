8c84e746f164cf009735b0a5f9a042e41823c7ea
Abhishek T <90185218+abhi1850@users.noreply.github.com>
2024-08-04 20:17:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/abhishek-my-portfolio/',
     host: '0.0.0.0',
     base: '/Abhishek-My-Portfolio/',
     resolve: {
