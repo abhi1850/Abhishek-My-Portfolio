d7cc45f1653c0c92c83ece51ff39b9a7d2a284b9
Abhishek T <abhishekthyagaraj@gmail.com>
2024-08-04 19:53:54 +0530

    Updated deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/Abhishek-My-Portfolio/',
     host: '0.0.0.0',
     resolve: {
         alias: {
