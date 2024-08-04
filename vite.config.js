6213c466c4ab4e744bcffde70a5b77825b3cd394
Abhishek T <abhishekthyagaraj@gmail.com>
2024-08-04 20:23:20 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     host: '0.0.0.0',
-    base: '/Abhishek-My-Portfolio/',
+    base: '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
